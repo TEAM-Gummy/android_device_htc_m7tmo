d34f21ad6946cba90825b8db2f4408ba1037611c
Andrew Warfield <hiasant@gmail.com>
2013-12-08 15:04:55 -0800

    Update PRODUCT_BUILD_PROP_OVERRIDES for 4.4 from GPE

--- tg.mk
+++ tg.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="tmous/m7/m7:4.1.2/JZO54K/170484.7:user/release-keys" PRIVATE_BUILD_DESC="1.27.531.7 CL170484 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=KRT16S.H5 BUILD_FINGERPRINT="tmous/m7/m7:4.4.1/KRT16S.H5/280886:user/release-keys" PRIVATE_BUILD_DESC="3.58.1700.5 CL280886 release-keys"
 
 PRODUCT_NAME := tg_m7tmo
 PRODUCT_DEVICE := m7tmo
